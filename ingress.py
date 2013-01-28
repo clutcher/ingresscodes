996afe24098cf3274aac2b957058374cc4c3c930
clutcher <iclutcher@gmail.com>
2013-01-28 10:59:57 +0200

    Added new pattern of code. Added cookie validation. Added pass search in G+ group and page of Decode Ingress.

--- ingress.py
+++ ingress.py
@@ -15,6 +15,14 @@ keyAPI = ''
 cookie = 'ACSID=' + acsid + ';' + 'csrftoken=' + csrftoken
 
 
+def checkInputData():
+    response = postToIntel('test')
+    if response.find('gameBasket'):
+        return 1
+    else:
+        return 0
+
+
 def convertTime(val):
     if '.' not in val:
         return datetime.datetime.strptime(val, "%Y-%m-%dT%H:%M:%S")
@@ -29,16 +37,19 @@ def collect(htmlSource):
     "Collect passcodes"
 
     passcode = []
-    # 3 patterns of passcodes
+    # 4 patterns of passcodes
     passFTypePattern = re.compile(
         "[0-9][A-Za-z][A-Za-z][0-9][A-Za-z]*[0-9][A-Za-z][0-9][A-Za-z]")
     passSTypePattern = re.compile("[A-Za-z]*[0-9][0-9][A-Za-z]")
     passTTypePattern = re.compile("82666[0-9]{5,6}")
+    passJTypePattern = re.compile("[A-Z]{10}")
+
     passcode.extend(re.findall(passFTypePattern, htmlSource))
     passcode.extend(re.findall(passSTypePattern, htmlSource))
     passcode.extend(re.findall(passTTypePattern, htmlSource))
+    passcode.extend(re.findall(passJTypePattern, htmlSource))
 
-    # Remove passcode with length < 4
+    # Remove passcode with length < 7
     for passOne in passcode:
         if len(passOne) < 7:
             passcode = filter(lambda a: a != passOne, passcode)
@@ -51,14 +62,26 @@ def parseGooglePlus():
     www = 'https://www.googleapis.com/plus/v1/activities'
     fields = 'items(object/content,updated)'
 
-    searchIPS = {'query': '#ingresspasscode', 'fields': fields, 'key': keyAPI}
-    searchPS = {'query': '#passcode', 'fields': fields, 'key': keyAPI}
+    searchIPS = {'query': '#ingresspasscode', 'orderBy': 'recent', 'maxResults': '20', 'fields': fields, 'key': keyAPI}
+    searchPS = {'query': '#passcode', 'orderBy': 'recent', 'maxResults': '20', 'fields': fields, 'key': keyAPI}
 
     rIPS = requests.get(www, params=searchIPS)
     rPS = requests.get(www, params=searchPS)
 
     data = rIPS.json()['items']
     data.extend(rPS.json()['items'])
+
+    # Because of strange working of Google+ API Search
+    # We will also fetch data from DeCode Ingress Group and Page
+    wwwG = 'https://www.googleapis.com/plus/v1/people/114606795989653285746/activities/public'
+    wwwP = 'https://www.googleapis.com/plus/v1/people/111701852247617430859/activities/public'
+    parameters = {'maxResults': '20', 'fields': fields, 'key': keyAPI}
+
+    rG = requests.get(wwwG, params=parameters)
+    rP = requests.get(wwwP, params=parameters)
+
+    data.extend(rP.json()['items'])
+    data.extend(rG.json()['items'])
 
     return data
 
@@ -79,36 +102,40 @@ def postToIntel(passcode):
 
 if __name__ == '__main__':
 
-    print 'Run time UTC0 - ' + str(datetime.datetime.now())
-    print 'Waiting for new passcodes...'
+     if checkInputData():
 
-    deltaTimeRegion = datetime.timedelta(hours=2)
-    deltaTimePost = datetime.timedelta(minutes=10)
+        print 'Run time UTC0 - ' + str(datetime.datetime.now())
+        print 'Waiting for new passcodes...'
 
-    accquired = []
+        deltaTimeRegion = datetime.timedelta(hours=2)
+        deltaTimePost = datetime.timedelta(minutes=10)
 
-    while True:
+        accquired = []
 
-        try:
-            data = parseGooglePlus()
-        except:
-            print 'Some magic in retrieving. Don`t worry.'
-            data = []
-        codes = []
+        while True:
 
-        for article in data:
-            date, content = article.values()
-            date = convertTime(date)
-            if (datetime.datetime.now() - date + deltaTimeRegion) < deltaTimePost:
-                codes = collect(content.values()[0])
+            try:
+                data = parseGooglePlus()
+            except:
+                print 'Some magic in retrieving. Don`t worry.'
+                data = []
+            codes = []
 
-                if len(codes) > 0:
-                    for code in codes:
-                        if code not in accquired:
-                            print 'Accquired passcode: ' + str(code)
-                            try:
-                                print postToIntel(code)
-                                accquired.append(code)
-                            except:
-                                print 'Malfunction. We`ll try again after one minute.'
-        time.sleep(60)
+            for article in data:
+                date, content = article.values()
+                date = convertTime(date)
+                if (datetime.datetime.now() - date + deltaTimeRegion) < deltaTimePost:
+                    codes = collect(content.values()[0])
+
+                    if len(codes) > 0:
+                        for code in codes:
+                            if code not in accquired:
+                                print 'Accquired passcode: ' + str(code)
+                                try:
+                                    print postToIntel(code)
+                                    accquired.append(code)
+                                except:
+                                    print 'Malfunction. We`ll try again after one minute.'
+            time.sleep(60)
+    else:
+        print "Invalid cookies."
